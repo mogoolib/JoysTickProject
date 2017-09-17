6ea9434b79bcc5b23c1ea794565d4d32320d5ac0
mogoolib <1040392895@qq.com>
2017-09-18 00:10:44 +0800

    add function
    加入控制屏幕旋转的功能

--- Assets/Scripts/ScreenControl.cs
+++ Assets/Scripts/ScreenControl.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class ScreenControl : MonoBehaviour {
+public class ScreenControl : MonoBehaviour{
 
     public Transform m_Cam;
 
@@ -18,8 +18,26 @@ public class ScreenControl : MonoBehaviour {
 
     }
 	
-	void Update ()
-    {
+    Vector3 pos;
+    bool isPointerDowwn;
 
-	}
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            isPointerDowwn = true;
+            pos = Input.mousePosition;
+            if (pos.x < transform.position.x) {
+                return;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && isPointerDowwn){
+            Debug.Log(transform.position);
+            float distance = (Input.mousePosition - pos).x;
+            float ditrction = distance > 0 ? 1.0f : -1.0f;
+            float tmpDis = Mathf.Abs(distance) / 300.0f * 100;
+            m_Cam.Rotate(Vector3.up, ditrction * tmpDis * 1.0f * Time.deltaTime, Space.World);
+        }
+    }
+
 }
