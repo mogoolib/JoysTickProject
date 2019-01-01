c269bf81b770b6a588d59704e93902415e7567c8
mogoolib <jinshan0421@hotmail.com>
2019-01-01 23:57:00 +0800

    change joystick script
    整理一下摇杆脚本

---
+++ Assets/Scripts/UI/UIJoystickControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIJoystickControl : MonoBehaviour {
+
+    public delegate void JoystickDragDelegate(Vector3 coor);
+    public JoystickDragDelegate joystickDragDelegate;
+
+    public delegate void JoystickDropDelegate();
+    public JoystickDropDelegate joystickDropDelegate;
+
+
+    public Transform border;
+
+	private float moveDistance;
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		startPosition = transform.position;
+
+		moveDistance = Vector3.Distance(transform.position, border.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (joystickDragDelegate != null) {
+            if (Vector3.Distance(transform.position, startPosition) > 0.1f) {
+                Vector3 dir = (Input.mousePosition - startPosition).normalized;
+                joystickDragDelegate(dir);
+                print(dir);
+            }
+        }
+	}
+
+	//drag event callback
+	public void OnJoystickDrag()
+	{
+		if(Vector3.Distance(Input.mousePosition, startPosition) < moveDistance){
+			transform.position = Input.mousePosition;
+		}else{
+			Vector3 dir = (Input.mousePosition - startPosition).normalized;
+			transform.position = startPosition + moveDistance * dir;
+		}
+	}
+
+	//drop event callback
+	public void OnJoystickDrop()
+	{
+		transform.position = startPosition;
+
+        if (joystickDropDelegate != null) {
+            joystickDropDelegate();
+        }
+	}
+}
---
+++ Assets/Scripts/UI/UIScreenControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class UIScreenControl : MonoBehaviour{
+
+    public Transform m_Cam;
+
+	void Start ()
+    {
+
+        if (m_Cam != null)
+        {
+            m_Cam = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Warning: no main camera found");
+        }
+
+    }
+	
+    Vector3 pos;
+    bool isPointerDowwn;
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            isPointerDowwn = true;
+            pos = Input.mousePosition;
+            if (pos.x < transform.position.x) {
+                return;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && isPointerDowwn){
+            Debug.Log(transform.position);
+            float distance = (Input.mousePosition - pos).x;
+            float ditrction = distance > 0 ? 1.0f : -1.0f;
+            float tmpDis = Mathf.Abs(distance) / 300.0f * 100;
+            m_Cam.Rotate(Vector3.up, ditrction * tmpDis * 1.0f * Time.deltaTime, Space.World);
+        }
+    }
+
+}
